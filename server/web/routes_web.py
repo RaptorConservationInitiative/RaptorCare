1b3dc0798f0aa780f05896991ed761c11ab98551
RaptorConservationInitiative <git@raptorconservationinitiative.de>
2026-05-19 13:21:48 +0200

    Update routes_web.py

--- server/web/routes_web.py
+++ server/web/routes_web.py
@@ -5,6 +5,9 @@ from server.auth.jwt import create_token
 from server.storage.db import get_conn
 from psycopg2.extras import RealDictCursor
 import os
+from fastapi import Depends
+from server.auth.dependencies import require_user
+
 
 router = APIRouter()
 
@@ -39,17 +42,16 @@ def login(username: str = Form(...), password: str = Form(...)):
     # TODO: password check (abhängig von deinem hashing)
     token = create_token({"user_id": user[0]})
 
-    return {
-        "access_token": token,
-        "token_type": "bearer"
-    }
+    response = RedirectResponse(url="/dashboard", status_code=302)
+    response.set_cookie(key="token", value=token, httponly=True)
+    return response
 
 
 # -------------------
 # DASHBOARD
 # -------------------
 @router.get("/dashboard")
-def dashboard(request: Request):
+def dashboard(request: Request, user=Depends(require_user)):
 
     conn = get_conn()
     cur = conn.cursor(cursor_factory=RealDictCursor)
