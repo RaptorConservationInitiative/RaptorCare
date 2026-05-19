37b5305bbf8ff1ee0af35921af9b9cafe3ca458b
RaptorConservationInitiative <git@raptorconservationinitiative.de>
2026-05-19 15:16:18 +0200

    Update dependencies.py

--- server/auth/dependencies.py
+++ server/auth/dependencies.py
@@ -1,7 +1,8 @@
 from fastapi import Request, HTTPException
 from server.auth.jwt import verify_token
-
+"""
 def require_user(request: Request):
+
     token = request.cookies.get("token")
 
     if not token:
@@ -13,3 +14,21 @@ def require_user(request: Request):
         raise HTTPException(status_code=401, detail="Invalid token")
 
     return payload
+"""
+
+def require_user(
+    request: Request,
+    authorization: str | None = Header(default=None)
+):
+    token = None
+
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
+
+    if not token:
+        token = request.cookies.get("token")
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Missing token")
+
+    return token
